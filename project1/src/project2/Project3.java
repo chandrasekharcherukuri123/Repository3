6e6d6b39bcb93e9334d539265a24fdfc9830067e
chandrasekhar009 <31537403+chandrasekhar009@users.noreply.github.com>
2017-10-11 13:19:57 +0530

    commit2

---
+++ project1/src/project2/Project3.java
@@ -0,0 +1,16 @@
+package project2;
+
+import java.util.Scanner;
+
+public class Project3 
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter username");
+		String u=sc.nextLine();
+		System.out.println("enter password");
+		String p=sc.nextLine();
+		//commit
+	}
+}
